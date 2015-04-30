602cc920c4b13bab7929bd19794c0b085bdd0d33
Tristan Harward <trisweb@gmail.com>
2015-04-29 23:29:50 -0400

    Placeholder and simple styles for displaying status

--- js/crapp.js
+++ js/crapp.js
@@ -21,18 +21,44 @@ crapp.config(
 .service('doorService',
   function() {
     return {
+      stallsAvailable: function(gender) {
+        return 1;
+      },
+      totalStalls: function(gender) {
+        return 3;
+      }
     }
   })
 
 // View Main
 .controller('MainController',
   function($scope, doorService) {
-
   })
 
 // View Settings
-.controller('SettingsController',
-  function($scope, doorService) {
+.controller('CommodeController',
+  function($scope, doorService, $routeParams) {
+    $scope.gender = $routeParams.gender;
+    $scope.numberOpen = doorService.stallsAvailable($scope.gender);
+    $scope.total = doorService.totalStalls($scope.gender);
+
+
+  })
+
+.filter('humanize', 
+  function() {
+    return function(text) {
+      if (text) {
+        text = text.split("_");
+        for (var i in text) {
+            var word = text[i];
+            word = word.toLowerCase();
+            word = word.charAt(0).toUpperCase() + word.slice(1);
+            text[i] = word;
+        }
+        return text.join(" ");
+      }
+    };
   })
 
 ;
