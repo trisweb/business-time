851c4b7a01ef0c492a8364bb05f544f7cfcc2fd6
Tristan Harward <trisweb@gmail.com>
2015-05-07 07:59:33 -0400

    Use memcached instead of local cache, and fix callbacks for jsonp

--- api/app.rb
+++ api/app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'net/http'
+require 'memcached'
 require './config'
 
 # Time to cache in seconds (a maximum of one request per POLL_TIME will be made
@@ -9,8 +10,8 @@ POLL_TIME = 1
 class Cache
 	@@cache = {}
 
-	def self.put(k, v)
-		#puts "Cache.put(#{k}, #{v})"
+	def self.set(k, v)
+		#puts "Cache.set(#{k}, #{v})"
 		@@cache[k] = v
 	end
 
@@ -21,21 +22,28 @@ class Cache
 end
 
 get "/" do
+	# cache = Cache
+	cache = Memcached.new("localhost:11211")
 	content_type :json
-	response = nil
+	result = nil
 	# If a cached key exists and it's less than POLL_TIME ms old, use it
-	response_updated_at = Cache.get('response_updated_at') || 0
+	response_updated_at = cache.get('response_updated_at') || 0
 	response_valid = Time.now.to_f - response_updated_at <= POLL_TIME
-	if response_valid || Cache.get('request_in_progress')
-		response = Cache.get 'response'
+	if response_valid || cache.get('request_in_progress')
+		response.headers[:CachedResponse] = "true"
+		result = cache.get 'response'
 	else
-		Cache.put 'request_in_progress', true
-	    uri = URI(BusinessTimeConfig.api_endpoint + "&callback=" + params['callback'].to_s)
+		cache.set 'request_in_progress', true
+	    uri = URI(BusinessTimeConfig.api_endpoint + "&callback=")
 	    content_type 'application/json', :charset => 'utf-8'
-	    response = Net::HTTP.get(uri)
-	    Cache.put 'response', response
-	    Cache.put 'response_updated_at', Time.now.to_f
-	    Cache.put 'request_in_progress', false
+	    result = Net::HTTP.get(uri)
+	    cache.set 'response', response
+	    cache.set 'response_updated_at', Time.now.to_f
+	    cache.set 'request_in_progress', false
 	end
-	response
+	# Prepend the requested callback (Angular rotates it)
+	if callback = params['callback'].to_s
+		result = callback + result
+	end
+	result
 end
