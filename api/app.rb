23f6d250e19edefd700f5f6bc4dc2319866c7f8a
Tristan Harward <trisweb@gmail.com>
2015-05-07 23:08:34 -0400

    Use a common cache client instance

--- api/app.rb
+++ api/app.rb
@@ -5,22 +5,22 @@ require './config'
 
 # Time to cache in seconds (a maximum of one request per POLL_TIME will be made
 # to the upstream server)
-POLL_TIME = 1
+POLL_TIME = 2
 
 get "/" do
 	# cache = Cache
-	cache = Memcached.new("localhost:11211")
+	cache = settings.cache
 	result = nil
 	# If a cached key exists and it's less than POLL_TIME ms old, use it
 	result_updated_at = cache.get('result_updated_at') rescue 0
 	request_in_progress = cache.get('request_in_progress') rescue false
 	result_valid = Time.now.to_f - result_updated_at <= POLL_TIME
 	if result_valid || request_in_progress
-		puts "=== Serving from Cache! ==="
 		response.headers['Cached-Response'] = "true"
 		result = cache.get 'result' rescue nil
 	else
 		cache.set 'request_in_progress', true
+    puts "=== Requesting from external API ==="
 		begin
 	    uri = URI(BusinessTimeConfig.api_endpoint + "&callback=")
 	    content_type 'application/json', :charset => 'utf-8'
--- api/config.ru
+++ api/config.ru
@@ -1,2 +1,6 @@
 require './app'
+
+cache = Memcached.new("localhost:11211")
+set :cache, cache
+
 run Sinatra::Application
